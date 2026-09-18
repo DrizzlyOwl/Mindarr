5bab12968c80e61eb690326bec17ecbfc444baff
Ash Davies <ash.davies@dxw.com>
2026-09-18 14:39:41 +0100

    Bump version to 1.4.0

--- plex_recommender/__init__.py
+++ plex_recommender/__init__.py
@@ -1,6 +1,6 @@
 """Mindarr: watch-history analyzer and unseen content discovery engine."""
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 from plex_recommender.logs import setup_logging, log_handler, LogHandler
 
